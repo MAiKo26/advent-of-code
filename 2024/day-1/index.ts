db44d8bde9aa3038a96dd82ffba1fdea9422c9fc
Mohamed Aziz Karoui <medazizkaroui26@gmail.com>
2024-12-02 18:56:20 +0100

    day 1

--- 2024/day-1/index.ts
+++ 2024/day-1/index.ts
@@ -1,7 +1,8 @@
-const fs = require("fs");
-const readline = require("readline");
-var file = "./input.txt";
-var pointer = readline.createInterface({
+import * as readline from "readline";
+import * as fs from "fs";
+
+const file = "./input.txt";
+const pointer = readline.createInterface({
   input: fs.createReadStream(file),
 });
 
@@ -11,7 +12,27 @@ const ListB: number[] = [];
 pointer.on("line", function (text: string) {
   ListA.push(Number(text.trim().split("   ")[0]));
   ListB.push(Number(text.trim().split("   ")[1]));
+});
 
-  console.log(ListA);
-  console.log(ListB);
+let sum = 0;
+let sum2 = 0;
+
+pointer.on("close", () => {
+  ListA.sort();
+  ListB.sort();
+
+  ListA.map((item, index) => {
+    sum = sum + Math.abs(item - ListB[index]);
+  });
+
+  for (let i = 0; i < ListA.length - 1; i++) {
+    let h = 0;
+    for (let j = 0; j < ListB.length; j++) {
+      if (ListA[i] === ListB[j]) h++;
+    }
+    sum2 = sum2 + ListA[i] * h;
+  }
+
+  console.log(sum);
+  console.log(sum2);
 });
